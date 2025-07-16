26f2a37c022560f10d2ff28ce47f8361611d6cff
patyhelenaa <patyhelena160@gmail.com>
2025-07-16 01:56:37 -0300

    chore: configura jest e atualiza dependências de teste

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+};
